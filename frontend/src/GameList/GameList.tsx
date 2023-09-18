e55b1c48b4f17d6d9faa4fe2b00eb664f2142456
juliancelso <juliancelsoginzburg@gmail.com>
2023-09-18 18:08:31 -0300

    Change the fetched in the frontend

--- frontend/src/GameList/GameList.tsx
+++ frontend/src/GameList/GameList.tsx
@@ -8,9 +8,35 @@ import axios from 'axios'
 const baseUrl = 'http://127.0.0.1:8080/api/'
 
 export function GameList(){
-    const getGames = () => axios.get(baseUrl + 'games').then(res => res.data)
-    const { data, isLoading, isError, error } = useQuery({ queryKey: ['games'], queryFn: getGames })
-    if(data?.games) return data.games.map((game: Game) => <GameCard game={game} key={game.name}/>)
-    else return <p>Cargando...</p>
-    
+
+    /**
+     * POSTGAME: we get the games from the backend and send they to the frontend to show them 
+     */
+
+
+
+    /**
+     * GETALLGAMES: Fetch games from the backend and display them on the frontend
+     */
+
+    // Define the getGames function that makes a GET request to the backend
+    const getGames = () => axios.get(baseUrl + 'games').then(res => res.data);
+
+    // Use React Query's useQuery to make the GET request
+    const { data, isLoading, isError, error } = useQuery({ queryKey: ['games'], queryFn: getGames });
+
+    // If isLoading is true, display a loading message
+    if (isLoading) return <p>Cargando...</p>;
+
+    // If isError is true, display an error message
+    if (isError) return <p>Error: {error.message}</p>;
+
+    // If data.games contains games, display them using GameCard components
+    if (data?.games?.length) {
+    return data.games.map((game: Game) => (
+        <GameCard game={game} key={game.name} />
+    ));
+    } else {
+    return <p>No se ha encontrado ningún juego.</p>;
+    }
 }
