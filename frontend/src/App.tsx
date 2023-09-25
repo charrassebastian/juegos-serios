d9324f218b181bbdd15baad0fce5743b00b7fb37
Sebastian Charras <sebastianhoraciocharras@gmail.com>
2023-09-25 18:23:50 +0000

    Solved warnings in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,10 @@
 import './App.css'
 import { GameList } from './GameList/GameList';
-import { games } from './exampleGames'
 
 /**
  * TANSTACK CONNECTION TO THE BACKEND FROM THE FRONTEND
  */
 import {
-  useQuery,
-  useMutation,
-  useQueryClient,
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
